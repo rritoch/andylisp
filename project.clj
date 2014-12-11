eada7996a48cb4490cc52e91d5b26dd7a88905d8
rritoch <rritoch@gmail.com>
2014-12-11 22:49:24 +0800

    Added project file

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject com.vnetpublishing/clojureplus "0.1.0-SNAPSHOT"
+  :description "Clojure Proof of Concept projects"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
+  :prep-tasks ["compile" "javac"]
+  :resource-paths ["src/resources"]
+  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
+                 [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
+                 [org.clojure/test.generative "0.5.1" :exclusions [org.clojure/clojure]]
+                 [org.clojure/test.check "0.5.9" :exclusions [org.clojure/clojure]]]
+                  
+  :aot :all)
