0924dc6aa6f013d2ea47e19f4cb58ebf84190fbd
rritoch <rritoch@gmail.com>
2014-12-12 03:29:44 +0800

    Project definition updates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.vnetpublishing/clojureplus "0.1.0-SNAPSHOT"
+(defproject clojureplus "0.1.0-SNAPSHOT"
   :description "Clojure Proof of Concept projects"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -6,13 +6,13 @@
   :java-source-paths ["src/jvm"]
   :prep-tasks ["compile" "javac"]
   :resource-paths ["src/resources"]
-  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
-                 
-                 ;[org.codehaus.jsr166-mirror/extra166y "1.7.0"]
+  :dependencies [;[org.codehaus.jsr166-mirror/extra166y "1.7.0]
                  [org.clojure/test.generative "0.5.1" :exclusions [org.clojure/clojure]]
                  [org.clojure/test.check "0.5.9" :exclusions [org.clojure/clojure]]]
                   
   :aot :all
   
+  :main clojure.main
   
-  :profiles {:provided {:dependencies [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}})
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
+                                       [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}})
