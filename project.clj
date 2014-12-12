99f044710bbe5ee81389c2f06a6fbf33cd56f69b
rritoch <rritoch@gmail.com>
2014-12-12 13:01:33 +0800

    Added backup of original pom.xml

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject clojureplus "0.1.0-SNAPSHOT"
-  :description "Clojure Proof of Concept projects"
-  :license {:name "Eclipse Public License"
+ (defproject clojureplus "0.1.0-SNAPSHOT"
+   :description "Clojure Proof of Concept projects"
+   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/clj"]
+   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
-  :prep-tasks ["compile" "javac"]
-  :resource-paths ["src/resources"]
-  :dependencies [;[org.codehaus.jsr166-mirror/extra166y "1.7.0]
-                 [org.clojure/test.generative "0.5.1" :exclusions [org.clojure/clojure]]
-                 [org.clojure/test.check "0.5.9" :exclusions [org.clojure/clojure]]]
+   :prep-tasks ["compile" "javac"]
+   :resource-paths ["src/resources"]
+   :dependencies [;[org.codehaus.jsr166-mirror/extra166y "1.7.0]
+                  [org.clojure/test.generative "0.5.1" :exclusions [org.clojure/clojure]]
+                  [org.clojure/test.check "0.5.9" :exclusions [org.clojure/clojure]]]
                   
-  :aot :all
+   :aot :all
   
-  :main clojure.main
+   :main clojure.main
   
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
-                                       [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}})
+   :profiles {:provided {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
+                                        [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}})
